57306b8300b413ecabe5d092b089ec1d03abcf7e
Josue <21387996@uagro.mx>
2025-06-07 21:30:30 -0600

    bonito

--- js/main.js
+++ js/main.js
@@ -3,6 +3,11 @@ window.onload = () => {
     const btnBusqueda = document.querySelector('#buscar');//apunto al boton
     const info1 = document.querySelector('#info_api_1');//apunto al div con la primera parte de la informacion
     const info2 = document.querySelector('#info_api_2')
+
+
+    function manejando_nulls(valor){
+        return (valor === null || valor === undefined || valor === "") ? 'no disponible' : valor;
+    }
     
 
     btnBusqueda.addEventListener('click', () =>{
@@ -25,24 +30,24 @@ window.onload = () => {
 
             info1.innerHTML= `
             <h3>DATOS BASE</h3>
-            <p>IP: ${data.ip}</p>
-            <p>País: ${data.country}</p>
-            <p>Región: ${data.subdivision}</p>
-            <p>Ciudad: ${data.city}</p>
-            <p>Moneda: ${data.currency_code}</p>
-            <p>Codigo: ${data.country_code}</p>
-            <p>Continente: ${data.continent}</p>
+            <p>IP: ${manejando_nulls(data.ip)}</p>
+            <p>País: ${manejando_nulls(data.country)}</p>
+            <p>Región: ${manejando_nulls(data.subdivision)}</p>
+            <p>Ciudad: ${manejando_nulls(data.city)}</p>
+            <p>Moneda: ${manejando_nulls(data.currency_code)}</p>
+            <p>Codigo: ${manejando_nulls(data.country_code)}</p>
+            <p>Continente: ${manejando_nulls(data.continent)}</p>
             
             `
 
             info2.innerHTML = `
             <h3>Datos de contacto de abuso</h3>
-            <p>dirección: ${data.abuse.address}</p>
-            <p>código de país: ${data.abuse.country_code}</p>
-            <p>dirección de correo electrónico: ${data.abuse.email} </p>
-            <p>nombre del contacto: ${data.abuse.name} </p>
-            <p>rango de red: ${data.abuse.network} </p>
-            <p>número de teléfono: ${data.abuse.phone} </p>
+            <p>dirección: ${manejando_nulls(data.abuse.address)}</p>
+            <p>código de país: ${manejando_nulls(data.abuse.country_code)}</p>
+            <p>dirección de correo electrónico: ${manejando_nulls(data.abuse.email)} </p>
+            <p>nombre del contacto: ${manejando_nulls(data.abuse.name)} </p>
+            <p>rango de red: ${manejando_nulls(data.abuse.network)} </p>
+            <p>número de teléfono: ${manejando_nulls(data.abuse.phone)} </p>
             
             `
         })
